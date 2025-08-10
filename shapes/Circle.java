924b42f9b228e8638ed6c3d87218f13a7ad827f5
Parth614 <guptaparth519@gmail.com>
2025-08-10 20:54:30 +0530

    Add files via upload

---
+++ shapes/Circle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Circle implements Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
